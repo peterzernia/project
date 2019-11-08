7f59f209b42ee05b6595d67e6e8c12e14f41f629
peterzernia <peter.zernia@medigo.com>
2019-11-08 13:31:31 +0100

    env variables from webpack inline env

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,38 +1,39 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const DotenvPlugin = require('webpack-dotenv-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
-  output: {
-    path: path.join(__dirname, '/build'),
-    filename: 'bundle.min.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
-      },
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = env => {
+  return {
+    entry: './src/index.tsx',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    output: {
+      path: path.join(__dirname, '/build'),
+      filename: 'bundle.min.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'awesome-typescript-loader',
         },
-      },
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(env),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new DotenvPlugin({
-      sample: './.env.dist',
-      path: './.env'
-    }),
-  ],
+  }
 }
